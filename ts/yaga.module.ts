64c5fe3a7245d006efacbcec93868362c06cb72a
Arne Schubert <atd.schubert@gmail.com>
2016-11-06 20:38:38 +0100

    #7: Make a angular module as entry-point for later usage in other projects

---
+++ ts/yaga.module.ts
@@ -0,0 +1,7 @@
+import { NgModule }      from '@angular/core';
+// import { MapComponent }   from './map.component';
+@NgModule({
+    declarations: [ /*MapComponent*/ ],
+    exports: [ /*MapComponent*/ ]
+})
+export class YagaModule { }
