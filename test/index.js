8358abcff6b5f19f9f2dcc474faf7d89210cb23f
Arne Schubert <atd.schubert@gmail.com>
2016-11-06 15:21:17 +0100

    #3: Enhance for unit tests run in node.js environment

--- test/index.js
+++ test/index.js
@@ -1,3 +1,8 @@
+if (typeof window === 'undefined') {
+    // load jsdom in tests running on node.js
+    require('jsdom-global')();
+}
+
 require('reflect-metadata');
 
 // require('../lib/.directive.spec');
