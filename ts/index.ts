6254eee22f62735451c58869fc39fe21cd706ecc
Stephan Herritsch <steveherritsch@hotmail.com>
2016-12-21 11:50:53 +0100

    Update index.ts
    add icon directive

--- ts/index.ts
+++ ts/index.ts
@@ -3,14 +3,17 @@ export * from './map.component';
 export * from './tile-layer.directive';
 export * from './image-overlay.directive';
 export * from './popup.directive';
+export * from './icon.directive';
+export * from './div-icon.directive';
 export * from './tooltip.directive';
 
 export {
     MapOptions,
     TileLayerOptions,
     PopupOptions,
-    TooltipOptions,
-    ImageOverlayOptions,
+    TooltipOptions,  
+    ImageOverlayOptions,  
+    IconOptions,
     LatLngBoundsExpression,
     LatLngBounds,
     PointExpression,
