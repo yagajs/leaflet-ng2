a53efbd38520c446aea26ddcfa5326993b83deaf
Arne Schubert <atd.schubert@gmail.com>
2016-12-28 16:52:49 +0100

    #43: Add marker to ng-module

--- ts/yaga.module.ts
+++ ts/yaga.module.ts
@@ -4,9 +4,26 @@ import { MapComponent,
     PopupDirective,
     TileLayerDirective,
     IconDirective,
-    DivIconDirective } from './index';
+    DivIconDirective,
+    MarkerDirective } from './index';
 @NgModule({
-    declarations: [ MapComponent, TileLayerDirective, ImageOverlayDirective, PopupDirective, IconDirective, DivIconDirective ],
-    exports: [ MapComponent, TileLayerDirective, ImageOverlayDirective, PopupDirective, IconDirective, DivIconDirective ]
+    declarations: [
+        MapComponent,
+        TileLayerDirective,
+        ImageOverlayDirective,
+        PopupDirective,
+        IconDirective,
+        DivIconDirective,
+        MarkerDirective
+    ],
+    exports: [
+        MapComponent,
+        TileLayerDirective,
+        ImageOverlayDirective,
+        PopupDirective,
+        IconDirective,
+        DivIconDirective,
+        MarkerDirective
+    ]
 })
 export class YagaModule { }
