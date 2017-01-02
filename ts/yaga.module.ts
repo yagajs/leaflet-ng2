147ca227dd1ea2a1e1061b2d1e3dacb091cd0927
Arne Schubert <atd.schubert@gmail.com>
2017-01-02 10:40:09 +0100

    Add polyline directive to the angular-module

--- ts/yaga.module.ts
+++ ts/yaga.module.ts
@@ -1,12 +1,37 @@
-import { NgModule }      from '@angular/core';
-import { MapComponent,
-    ImageOverlayDirective,
-    PopupDirective,
-    TileLayerDirective,
-    IconDirective,
-    DivIconDirective } from './index';
+import { NgModule } from '@angular/core';
+
+import { MapComponent } from './map.component';
+import { TileLayerDirective } from './tile-layer.directive';
+import { ImageOverlayDirective } from './image-overlay.directive';
+import { PopupDirective } from './popup.directive';
+import { TooltipDirective } from './tooltip.directive';
+import { IconDirective } from './icon.directive';
+import { DivIconDirective } from './div-icon.directive';
+import { MarkerDirective } from './marker.directive';
+import { PolylineDirective } from './polyline.directive';
+
 @NgModule({
-    declarations: [ MapComponent, TileLayerDirective, ImageOverlayDirective, PopupDirective, IconDirective, DivIconDirective ],
-    exports: [ MapComponent, TileLayerDirective, ImageOverlayDirective, PopupDirective, IconDirective, DivIconDirective ]
+    declarations: [
+        MapComponent,
+        TileLayerDirective,
+        ImageOverlayDirective,
+        PopupDirective,
+        TooltipDirective,
+        IconDirective,
+        DivIconDirective,
+        MarkerDirective,
+        PolylineDirective
+    ],
+    exports: [
+        MapComponent,
+        TileLayerDirective,
+        ImageOverlayDirective,
+        PopupDirective,
+        TooltipDirective,
+        IconDirective,
+        DivIconDirective,
+        MarkerDirective,
+        PolylineDirective
+    ]
 })
 export class YagaModule { }
