e6e340bd3fcce030491cf8c601adc97f6be0386e
Arne Schubert <atd.schubert@gmail.com>
2017-03-18 23:50:40 +0100

    #195: Integrate into example-design

--- examples/passau/index.ts
+++ examples/passau/index.ts
@@ -9,32 +9,42 @@ import { BrowserModule } from '@angular/platform-browser';
 import { FormsModule }   from '@angular/forms';
 import { platformBrowserDynamic } from '@angular/platform-browser-dynamic';
 
+import { ExamplePropertiesModule } from '../property.component'; // this is just for the header
+
 const platform: PlatformRef = platformBrowserDynamic();
 
 /* tslint:disable:max-line-length */
 const template: string = `
-<yaga-map [lat]="48.5768558" [lng]="13.268283" [zoom]="11">
+<example-header [title]="'FOSSGIS 2017 Example'"></example-header>
+<div class="container">
+  <div class="map">
+    <!-- Template-Example from presentation starts here -->
+    <yaga-map [lat]="48.5768558" [lng]="13.268283" [zoom]="11">
+      <yaga-zoom-control></yaga-zoom-control>
+      <yaga-scale-control [metric]="true" [imperial]="false"></yaga-scale-control>
+      <yaga-attribution-control></yaga-attribution-control>
+      
+      <yaga-tile-layer [url]="'http://{s}.tile.openstreetmap.org/{z}/{x}/{y}.png'" [attribution]="'© OpenStreetMap-Mitwirkende'"></yaga-tile-layer>
+      <yaga-marker [(lat)]="lat" [(lng)]="lng" [(draggable)]="editable">
+        <yaga-popup>
+          <h3>Hello World</h3>
+          <p>
+            This is <strong>{{ name }}</strong><br/><br/>
+            <em>Do you wan't to edit me?: <input type="checkbox" [(ngModel)]="editable"/></em> <br/>
+            <span *ngIf="editable">
+              Latitude: <input type="number" [(ngModel)]="lat" step="0.001" width="80"/><br/>
+              Longitude: <input type="number" [(ngModel)]="lng" step="0.001" width="80"/><br/>
+              This is: <input type="text" [(ngModel)]="name" width="80"/>
+            </span>
+          </p>
+        </yaga-popup>
+      </yaga-marker>
+    </yaga-map>
+    <!-- Example-Example from presentation ends here -->
+  </div>
+</div><!-- /.container -->
+<example-footer></example-footer>
 
-  <yaga-zoom-control></yaga-zoom-control>
-  <yaga-scale-control [metric]="true" [imperial]="false"></yaga-scale-control>
-  <yaga-attribution-control></yaga-attribution-control>
-
-  <yaga-tile-layer [url]="'http://{s}.tile.openstreetmap.org/{z}/{x}/{y}.png'" [attribution]="'© OpenStreetMap-Mitwirkende'"></yaga-tile-layer>
-  <yaga-marker [lat]="48.5768558" [lng]="13.268283" [(draggable)]="editable">
-    <yaga-popup>
-      <h3>Hello World</h3>
-      <p>
-        This is <strong>{{ name }}</strong><br/><br/>
-        <em>Do you wan't to edit me?: <input type="checkbox" [(ngModel)]="editable"/></em><br/>
-        <span *ngIf="editable">
-          Latitude: <input type="number" [(ngModel)]="lat" step="0.001" width="80"/><br/>
-          Longitude: <input type="number" [(ngModel)]="lng" step="0.001" width="80"/><br/>
-          This is: <input type="text" [(ngModel)]="name" width="80"/>
-        </span>
-      </p>
-    </yaga-popup>
-  </yaga-marker>
-</yaga-map>
 `;
 /* tslint:enable */
 
@@ -52,7 +62,7 @@ export class AppComponent {
 @NgModule({
     bootstrap:    [ AppComponent ],
     declarations: [ AppComponent ],
-    imports:      [ BrowserModule, FormsModule, YagaModule ]
+    imports:      [ BrowserModule, FormsModule, YagaModule, /* just for the website -> */ ExamplePropertiesModule ]
 })
 export class AppModule { }
 
