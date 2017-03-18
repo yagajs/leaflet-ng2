6a9bfc4cb3bef7a35449fa4b8f52087bfac01928
Arne Schubert <atd.schubert@gmail.com>
2017-03-18 22:57:54 +0100

    #195: Add controls

--- examples/passau/index.ts
+++ examples/passau/index.ts
@@ -14,6 +14,11 @@ const platform: PlatformRef = platformBrowserDynamic();
 /* tslint:disable:max-line-length */
 const template: string = `
 <yaga-map [lat]="48.5768558" [lng]="13.268283" [zoom]="11">
+
+      <yaga-zoom-control></yaga-zoom-control>
+      <yaga-scale-control [metric]="true" [imperial]="false"></yaga-scale-control>
+      <yaga-attribution-control></yaga-attribution-control>
+
   <yaga-tile-layer [url]="'http://{s}.tile.openstreetmap.org/{z}/{x}/{y}.png'" [attribution]="'© OpenStreetMap-Mitwirkende'"></yaga-tile-layer>
 </yaga-map>
 `;
