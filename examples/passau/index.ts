a0c6eb97f8e3fc2c167062a8dd3da078ba26fff7
Arne Schubert <atd.schubert@gmail.com>
2017-03-18 23:11:49 +0100

    #195: Add a marker with popup

--- examples/passau/index.ts
+++ examples/passau/index.ts
@@ -20,6 +20,14 @@ const template: string = `
   <yaga-attribution-control></yaga-attribution-control>
 
   <yaga-tile-layer [url]="'http://{s}.tile.openstreetmap.org/{z}/{x}/{y}.png'" [attribution]="'© OpenStreetMap-Mitwirkende'"></yaga-tile-layer>
+  <yaga-marker [lat]="48.5768558" [lng]="13.268283">
+    <yaga-popup>
+      <h3>Hello World</h3>
+      <p>
+        This is <strong>Passau</strong>
+      </p>
+    </yaga-popup>
+  </yaga-marker>
 </yaga-map>
 `;
 /* tslint:enable */
