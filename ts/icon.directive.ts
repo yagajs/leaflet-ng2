bcf2f685a789945e4ecdcddcf5b0ad67b947928a
Arne Schubert <atd.schubert@gmail.com>
2017-06-11 19:26:05 +0200

    #220: Add retina properties and className that was missing

--- ts/icon.directive.ts
+++ ts/icon.directive.ts
@@ -24,6 +24,16 @@ export class IconDirective extends Icon  {
         });
     }
 
+    @Input() public set className(val: string) {
+        this.options.className = val;
+        this.updateEvent.emit({
+            target: this,
+            type: 'update',
+        });
+    }
+    public get className(): string {
+        return this.options.className;
+    }
     @Input() public set iconUrl(val: string) {
         this.options.iconUrl = val;
         this.updateEvent.emit({
@@ -33,6 +43,16 @@ export class IconDirective extends Icon  {
     }
     public get iconUrl(): string {
         return this.options.iconUrl;
+    }
+    @Input() public set iconRetinaUrl(val: string) {
+        this.options.iconRetinaUrl = val;
+        this.updateEvent.emit({
+            target: this,
+            type: 'update',
+        });
+    }
+    public get iconRetinaUrl(): string {
+        return this.options.iconRetinaUrl;
     }
     @Input() public set iconSize(val: Point) {
         this.options.iconSize = val;
@@ -64,6 +84,19 @@ export class IconDirective extends Icon  {
     public get popupAnchor(): Point {
         return (<Point> this.options.popupAnchor);
     }
+    // We have to wait for adding the definition for tooltipAnchor
+    // see: https://github.com/yagajs/leaflet-ng2/issues/220#issuecomment-307634276
+    //
+    // @Input() public set tooltipAnchor(val: Point) {
+    //     this.options.tooltipAnchor = val;
+    //     this.updateEvent.emit({
+    //         target: this,
+    //         type: 'update',
+    //     });
+    // }
+    // public get tooltipAnchor(): Point {
+    //     return (<Point> this.options.popupAnchor);
+    // }
     @Input() public set shadowUrl(val: string) {
         this.options.shadowUrl = val;
         this.updateEvent.emit({
@@ -73,6 +106,16 @@ export class IconDirective extends Icon  {
     }
     public get shadowUrl(): string {
         return this.options.shadowUrl;
+    }
+    @Input() public set shadowRetinaUrl(val: string) {
+        this.options.shadowRetinaUrl = val;
+        this.updateEvent.emit({
+            target: this,
+            type: 'update',
+        });
+    }
+    public get shadowRetinaUrl(): string {
+        return this.options.shadowRetinaUrl;
     }
     @Input() public set shadowSize(val: Point) {
         this.options.shadowSize = val;
