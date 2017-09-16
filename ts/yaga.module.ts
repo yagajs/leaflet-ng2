cfc74a036d1875c27fa491e82ca5705d3e7c0a62
Arne Schubert <atd.schubert@gmail.com>
2017-09-16 19:43:09 +0200

    #250: Add a documentation for the module

--- ts/yaga.module.ts
+++ ts/yaga.module.ts
@@ -19,6 +19,21 @@ import { TooltipDirective } from './tooltip.directive';
 import { WmsLayerDirective } from './wms-layer.directive';
 import { ZoomControlDirective } from './zoom-control.directive';
 
+/**
+ * Angular 2++ module that you can use to import all directives of YAGA's leaflet-ng2 into your Angular application.
+ *
+ * ```
+ * import { NgModule } from '@angular/core';
+ * import { YagaModule } from '@yaga/leaflet-ng2';
+ *
+ * ; @NgModule({
+ *     imports: [
+ *         YagaModule,
+ *     ],
+ * })
+ * export class AppModule {}
+ * ```
+ */
 @NgModule({
     declarations: [
         AttributionControlDirective,
